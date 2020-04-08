dc755a37b1adf4fda4d5ce4a52b073c908b3ffac
pankhuri03 <43607143+pankhuri03@users.noreply.github.com>
2020-04-09 02:18:36 +0530

    Add files via upload

---
+++ chats/src/chats/chat_client.java
@@ -0,0 +1,349 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chats;
+
+/**
+ *
+ * @author Abhinand
+ */
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.net.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.border.Border;
+
+public class chat_client extends javax.swing.JFrame {
+    
+    static Socket s;
+    static DataInputStream din;
+    static DataOutputStream dout;
+    
+    public chat_client() {
+        initComponents();
+    }
+    //--------------------------------------------------------------------------
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        msg_area = new javax.swing.JTextArea();
+        msg_text = new javax.swing.JTextField();
+        msg_send = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabelclose = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocation(new java.awt.Point(100, 100));
+        setUndecorated(true);
+
+        jPanel1.setBackground(new java.awt.Color(33, 33, 33));
+
+        jPanel2.setBackground(new java.awt.Color(33, 33, 33));
+
+        msg_area.setBackground(new java.awt.Color(44, 62, 80));
+        msg_area.setColumns(20);
+        msg_area.setFont(new java.awt.Font("Comic Sans MS", 0, 13)); // NOI18N
+        msg_area.setForeground(new java.awt.Color(255, 255, 255));
+        msg_area.setLineWrap(true);
+        msg_area.setRows(5);
+        msg_area.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jScrollPane1.setViewportView(msg_area);
+
+        msg_text.setBackground(new java.awt.Color(44, 62, 80));
+        msg_text.setForeground(new java.awt.Color(255, 255, 255));
+        msg_text.setToolTipText("");
+        msg_text.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        msg_text.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                msg_textActionPerformed(evt);
+            }
+        });
+
+        msg_send.setBackground(new java.awt.Color(33, 33, 33));
+        msg_send.setText("Send");
+        msg_send.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                msg_sendActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(msg_text, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(msg_send, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addContainerGap())))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 609, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(msg_text, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(msg_send, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText(" Chat");
+        jLabel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jLabel1MouseDragged(evt);
+            }
+        });
+
+        jPanel3.setBackground(new java.awt.Color(255, 0, 0));
+
+        jLabelclose.setFont(new java.awt.Font("Malgun Gothic", 0, 24)); // NOI18N
+        jLabelclose.setText("X");
+        jLabelclose.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabelcloseMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelclose)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabelclose))
+        );
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/question_answer_white_36x36 (2).png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(120, 120, 120)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+    //--------------------------------------------------------------------------
+    private void msg_sendActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_msg_sendActionPerformed
+      
+      try{  
+          
+          DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");  
+            LocalDateTime now = LocalDateTime.now();
+      
+            String msgout="";
+      
+            msgout=msg_text.getText().trim();
+            dout.writeUTF(msgout);
+            if(!msgout.equals(""))
+            msg_area.setText(msg_area.getText()+"\n"+dtf.format(now)+"     You: "+msgout);
+            msg_text.setText("");
+        }
+      catch(Exception e){
+          
+      }
+      
+    }//GEN-LAST:event_msg_sendActionPerformed
+    //--------------------------------------------------------------------------
+    private void msg_textActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_msg_textActionPerformed
+        
+    }//GEN-LAST:event_msg_textActionPerformed
+    //--------------------------------------------------------------------------
+    private void jLabelcloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelcloseMouseClicked
+      System.exit(0);
+    }//GEN-LAST:event_jLabelcloseMouseClicked
+    //--------------------------------------------------------------------------
+    private void jLabel1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x, y);
+    }//GEN-LAST:event_jLabel1MouseDragged
+
+    
+    //--------------------------------------------------------------
+    public class SubtleSquareBorder implements Border
+{
+protected int m_w = 6;
+protected int m_h = 6;
+protected Color m_topColor = Color.white;
+protected Color m_bottomColor = Color.gray;
+protected boolean roundc = false; // Do we want rounded corners on the border?
+public SubtleSquareBorder(boolean round_corners)
+{
+                roundc = round_corners;
+}
+public Insets getBorderInsets(Component c)
+{
+return new Insets(m_h, m_w, m_h, m_w);
+}
+public boolean isBorderOpaque()
+{
+return true;
+}
+public void paintBorder(Component c, Graphics g, int x, int y, int w, int h)
+{
+w = w - 3;
+h = h - 3;
+x ++;
+y ++;
+// Rounded corners
+if(roundc)
+{
+g.setColor(m_topColor);
+g.drawLine(x, y + 2, x, y + h - 2);
+g.drawLine(x + 2, y, x + w - 2, y);
+g.drawLine(x, y + 2, x + 2, y); // Top left diagonal
+g.drawLine(x, y + h - 2, x + 2, y + h); // Bottom left diagonal
+g.setColor(m_bottomColor);
+g.drawLine(x + w, y + 2, x + w, y + h - 2);
+g.drawLine(x + 2, y + h, x + w -2, y + h);
+g.drawLine(x + w - 2, y, x + w, y + 2); // Top right diagonal
+g.drawLine(x + w, y + h - 2, x + w -2, y + h); // Bottom right diagonal
+}
+// Square corners
+else
+{
+g.setColor(m_topColor);
+g.drawLine(x, y, x, y + h);
+g.drawLine(x, y, x + w, y);
+g.setColor(m_bottomColor);
+g.drawLine(x + w, y, x + w, y + h);
+g.drawLine(x, y + h, x + w, y + h);
+}
+}
+}
+   
+    //--------------------------------------------------------------
+    
+    public static void main(String args[]) {
+        
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(chat_client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(chat_client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(chat_client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(chat_client.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new chat_client().setVisible(true);
+            }
+        });
+        
+        try
+        {  
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");  
+            LocalDateTime now = LocalDateTime.now();
+            
+            s=new Socket("localhost",1203);
+            
+            din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
+            
+            String msgin="";
+            
+            while(!msgin.equals("exit"))
+            {
+                
+                msgin=din.readUTF();
+                if(!msgin.equals("")){
+                    msg_area.setText(msg_area.getText().trim()+"\n"+dtf.format(now)+" Friend: "+msgin);
+                }
+            }
+        }
+        catch(IOException e){
+            
+        }
+        
+    }
+//------------------------------------------------------------------------------
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabelclose;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private static javax.swing.JTextArea msg_area;
+    public javax.swing.JButton msg_send;
+    private javax.swing.JTextField msg_text;
+    // End of variables declaration//GEN-END:variables
+}
